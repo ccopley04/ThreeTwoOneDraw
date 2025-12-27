12890e4fb911c739fa7f12a68966f2b2f82de8e7
ccopley04 <ccopley7@gatech.edu>
2025-12-27 11:18:31 -0500

    Main menu finished
    Altered Menu Manager to not be time based. Instead, used animation events. The animations that the buttons played call the Menu Manager's method when a specific frame is reached. With this, the timings of sounds and method calls can be specific. The main menu buttons now work and call the correct methods.

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -12,89 +12,40 @@ public class MenuManager : MonoBehaviour
 {
 
     public List<GameObject> buttons = new List<GameObject>();
-    public GameObject startButton;
     public List<GameObject> UIObjects = new List<GameObject>();
     public List<GameObject> credits = new List<GameObject>();
 
-    public static MenuManager Instance { get; private set; }
+    public static MenuManager Instance;
+    private Sprite firstSprite;
 
-    public Animator startAnimator;
-
-    public void Awake()
-        {
-            if (Instance != null && Instance != this)
-            {
-                Destroy(this);
-            }
-            else
-            {
-                Instance = this;
-                MusicManager.playSound(MusicType.Theme, 0.5F);
-                MusicManager.audioSource.loop = true;
-            }
+    public void Awake() {
+        if (Instance == null) {
+            Instance = gameObject.GetComponent<MenuManager>();
         }
+
+        firstSprite = gameObject.GetComponent<SpriteRenderer>().sprite;
+    }
 
     public void Quit() {
-        StartCoroutine(WaitToQuitGame());
+        Application.Quit();
     }
-
-    public IEnumerator WaitToQuitGame() {
-            bool shot = false;
-
-            float duration = 1.2F;
-            while (duration > 0)
-            {
-                //Alter the time by the time since last frame
-                duration -= Time.deltaTime;
-                if (duration <= 0)
-                {
-                    duration = 0;
-                } else if (duration <= 0.7F && !shot) {
-                    shot = true;
-                    SoundManager.playSound(SoundType.SixShooterBullet);
-                }
-                yield return null;
-            }
-            Application.Quit();
-        }
-
 
     public void StartGame() {
-        StartCoroutine(WaitToStartGame());
-    }
-
-    public IEnumerator WaitToStartGame() {
-        bool shot = false;
-        Sprite firstSprite = startButton.GetComponent<SpriteRenderer>().sprite;
-        float duration = 2.50F;
-        while (duration > 0)
-        {
-            //Alter the time by the time since last frame
-            duration -= Time.deltaTime;
-            if (duration <= 0)
-            {
-                duration = 0;
-            } else if (duration <= 1.45F && !shot) {
-                shot = true;
-                SoundManager.playSound(SoundType.SixShooterBullet);
-            }
-            yield return null;
-        }
         if (!SceneManager.GetSceneByName("CombatDemo").isLoaded)
-        {
-            SceneManager.LoadScene("CombatDemo", LoadSceneMode.Additive);
-        }
-        if (!SceneManager.GetSceneByName("OverWEug").isLoaded)
-        {
-            SceneManager.LoadScene("OverWEug", LoadSceneMode.Additive);
-        } else {
-            DisableOverworld.Instance.enableOverworld(true);
-        }
+                {
+                    SceneManager.LoadScene("CombatDemo", LoadSceneMode.Additive);
+                }
+                if (!SceneManager.GetSceneByName("OverWEug").isLoaded)
+                {
+                    SceneManager.LoadScene("OverWEug", LoadSceneMode.Additive);
+                } else {
+                    DisableOverworld.Instance.enableOverworld(true);
+                }
 
-        foreach (GameObject button in buttons) {
-            button.SetActive(false);
-        }
-        startButton.GetComponent<SpriteRenderer>().sprite = firstSprite;
+                foreach (GameObject button in buttons) {
+                    button.SetActive(false);
+                }
+        gameObject.GetComponent<SpriteRenderer>().sprite = firstSprite;
     }
 
     public void ReturnToMenu() {
@@ -103,22 +54,30 @@ public class MenuManager : MonoBehaviour
        }
     }
 
+    public void PlayShot() {
+        SoundManager.playSound(SoundType.SixShooterBullet);
+    }
+
 
     public void StartCredits() {
         foreach (GameObject credit in credits) {
-            credit.SetActive(true);
-        }
-        foreach (GameObject uiObject in UIObjects) {
-            uiObject.SetActive(false);
-        }
+                        credit.SetActive(true);
+                    }
+                    foreach (GameObject uiObject in UIObjects) {
+                        uiObject.SetActive(false);
+                    }
+        gameObject.GetComponent<SpriteRenderer>().sprite = firstSprite;
     }
+
+
 
     public void EndCredits() {
             foreach (GameObject credit in credits) {
-            credit.SetActive(false);
-            }
-            foreach (GameObject uiObject in UIObjects) {
-                uiObject.SetActive(true);
-            }
-        }
+                                           credit.SetActive(false);
+                                           }
+                                           foreach (GameObject uiObject in UIObjects) {
+                                               uiObject.SetActive(true);
+                                           }
+    gameObject.GetComponent<SpriteRenderer>().sprite = firstSprite;
+    }
 }
